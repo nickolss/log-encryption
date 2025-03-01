c15753b73e02030a9ffd4908f31cffcfc41ee542
nickolss <nickolasmaiaraujo@gmail.com>
2025-03-01 11:48:48 -0300

    feat: addeed dynamic encryption logic

--- src/log/logs.controller.ts
+++ src/log/logs.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, Post, Query } from '@nestjs/common';
 import { LogService } from './logs.service';
 import { ApiOperation, ApiTags } from '@nestjs/swagger';
 import { EncryptLogDto } from './dto/encryptLog';
@@ -11,13 +11,19 @@ export class LogsController {
 
   @Post('crypt')
   @ApiOperation({ summary: 'Encrypt log' })
-  encryptLog(@Body() log: EncryptLogDto): string {
-    return this.logService.encryptLog(log.log);
+  encryptLog(
+    @Body() log: EncryptLogDto,
+    @Query('secret') secret: string,
+  ): string {
+    return this.logService.encryptLog(log.log, secret);
   }
 
   @Post('decrypt')
   @ApiOperation({ summary: 'Decrypt log' })
-  decryptLog(@Body() cryptLog: DecryptLogDto): Record<string, any> {
-    return this.logService.decryptLog(cryptLog);
+  decryptLog(
+    @Body() cryptLog: DecryptLogDto,
+    @Query('secret') secret: string,
+  ): Record<string, any> {
+    return this.logService.decryptLog(cryptLog, secret);
   }
 }
--- src/log/logs.service.ts
+++ src/log/logs.service.ts
@@ -1,5 +1,4 @@
 import { Injectable } from '@nestjs/common';
-import { ConfigService } from '@nestjs/config';
 import {
   createCipheriv,
   createDecipheriv,
@@ -11,29 +10,25 @@ import { DecryptLogDto } from './dto/decryptLog';
 @Injectable()
 export class LogService {
   private algorithm = 'aes-256-cbc';
-  private secretKey: Buffer;
-  private iv = randomBytes(16);
 
-  constructor(private configService: ConfigService) {
-    const secret = this.configService.get<string>('SECRET_KEY');
-    if (!secret) {
-      throw new Error('Secret key not found');
-    }
+  encryptLog(log: Record<string, any>, secret: string): string {
+    const iv = randomBytes(16);
+    const secretKey = scryptSync(secret, 'salt', 32);
 
-    this.secretKey = scryptSync(secret, 'salt', 32);
-  }
-
-  encryptLog(log: Record<string, any>): string {
-    const cipher = createCipheriv(this.algorithm, this.secretKey, this.iv);
+    const cipher = createCipheriv(this.algorithm, secretKey, iv);
     const json = JSON.stringify(log);
     let encrypted = cipher.update(json, 'utf-8', 'hex');
     encrypted += cipher.final('hex');
-    return encrypted;
+    return iv.toString('hex') + ':' + encrypted;
   }
 
-  decryptLog(cryptLog: DecryptLogDto): Record<string, any> {
-    const decipher = createDecipheriv(this.algorithm, this.secretKey, this.iv);
-    let decrypted = decipher.update(cryptLog.encryptedLog, 'hex', 'utf8');
+  decryptLog(cryptLog: DecryptLogDto, secret: string): Record<string, any> {
+    const [ivHex, encryptedLog] = cryptLog.encryptedLog.split(':');
+    const iv = Buffer.from(ivHex, 'hex');
+    const secretKey = scryptSync(secret, 'salt', 32);
+
+    const decipher = createDecipheriv(this.algorithm, secretKey, iv);
+    let decrypted = decipher.update(encryptedLog, 'hex', 'utf8');
     decrypted += decipher.final('utf8');
 
     return JSON.parse(decrypted);
